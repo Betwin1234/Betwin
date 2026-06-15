44bec24da8e53b10e1e0fa99fab68edb329cc98e
Betwin1234 <addoa413@gmail.com>
2026-06-15 00:09:39 +0000

    Create games.js

---
+++ games.js
@@ -0,0 +1,15 @@
+const games = [
+  {
+    home: "Chelsea",
+    away: "Arsenal",
+    time: "18:00",
+    odds: "1.8"
+  },
+
+  {
+    home: "Real Madrid",
+    away: "Barcelona",
+    time: "20:00",
+    odds: "2.1"
+  }
+];
